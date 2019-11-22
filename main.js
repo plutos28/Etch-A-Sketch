65638127099585d8553edf0e1bc8d52416d20b93
plutos28 <gamevictor28@gmail.com>
2019-11-22 13:49:22 +0300

    add grid divs onto grid-container div

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 let grid = [];
+let gridContainer = document.querySelector('#grid-container');
 
 // add 32 divs to grid 
 for(let i = 0; i < 32; i++) {
-    let gridItem = document.createElement("div");
+    let gridItem = document.createElement('div');
     grid.push(gridItem); 
 }
 
+// add all the divs from grid to gridContainer
+// note: might need to sort the array in reverse
+// this is because the first items in grid will be the last items in grid-container
+grid.forEach((gridItem) => {
+    gridContainer.appendChild(gridItem);
+});
