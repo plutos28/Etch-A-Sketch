5c2d822ecdc45a3b1474c3f31f18194f010ba62e
plutos28 <gamevictor28@gmail.com>
2019-11-24 23:15:05 +0300

    make grid making into own function

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
-let grid = [];
-let gridContainer = document.querySelector('#grid-container');
+function createGrid(size) {
+    // todo: consider taking different rows and column sizes
+    let grid = [];
+    // add size divs to grid 
+    for(let i = 0; i < size; i++) {
+        let gridItem = document.createElement('div');
+        grid.push(gridItem); 
+    } 
 
-// add 32 divs to grid 
-for(let i = 0; i < 32; i++) {
-    let gridItem = document.createElement('div');
-    grid.push(gridItem); 
+    return grid;
 }
 
+const gridContainer = document.querySelector('#grid-container');
+
 // add all the divs from grid to gridContainer
-// note: might need to sort the array in reverse
-// this is because the first items in grid will be the last items in grid-container
-grid.forEach((gridItem) => {
+createGrid(16).forEach((gridItem) => {
     gridContainer.appendChild(gridItem);
 });
+
+// todo: give gridContainer an apprioate sized box that looks nice
+// make its borders appear nicely
+
+// todo: turn gridContainer into a css grid
+// make it 16 rows and 16 columns with the size being fractional(1fr)
+
