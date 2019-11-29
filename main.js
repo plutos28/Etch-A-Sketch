e3b8d1433d1505f4da7414a1f5c2c57fc63033a0
plutos28 <48158371+plutos28@users.noreply.github.com>
2019-11-29 23:13:00 +0300

    use black as default color and create random color through button

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
-function createGrid(size, gridItemColors) {
+function createGrid(size) {
     let grid = [];
     for(let i = 0; i < size; i++) {
-        grid.push(createGridItem(gridItemColors)); 
+        grid.push(createGridItem()); 
     } 
 
     return grid;
 }
 
-function createGridItem(color=[0, 0, 0]) {
+function createGridItem() {
     const gridItem = document.createElement('div');
-    gridItem.style.border = `1px solid hsl(0, 0%, 0%)`;
+    gridItem.style.border = '1px solid hsl(0, 0%, 0%)';
 
     gridItem.addEventListener('mouseenter', () => {
-        gridItem.style.backgroundColor = `hsl(${color[0]}, ${color[1]}%, ${color[2]}%)`;
+        gridItem.style.backgroundColor = 'hsl(0, 0%, 0%)';
     });
 
     return gridItem;
 }
 
-function drawGrid(gridRows=16, gridColumns=16, gridWidth=35, gridHeight=35, color=[0, 0, 0]) {
+function drawGrid(gridRows=16, gridColumns=16, gridWidth=35, gridHeight=35) {
     // add the gridItems to the dom through putting them
     // in the gridContainer div and then draw it
     const gridContainer = document.querySelector('#grid-container');
     const gridSize = gridRows * gridColumns; // meaning gridRows by gridColumns box 
 
-    createGrid(gridSize, color).forEach((gridItem) => {
+    createGrid(gridSize).forEach((gridItem) => {
         gridContainer.appendChild(gridItem);
     });
 
@@ -61,6 +61,17 @@ function getRandHSL() {
     return [hue, saturation, lightness];
 }
 
+function useRandomColor() {
+    const color = getRandHSL();
+    const gridContainer = document.querySelector('#grid-container');
+
+    gridContainer.childNodes.forEach((gridItem) => {
+        gridItem.addEventListener('mouseenter', () => {
+            gridItem.style.backgroundColor = `hsl(${color[0]}, ${color[1]}%, ${color[2]}%)`;
+        });
+    });
+}
+
 function play() {
     // draw initial grid which is 16x16
     drawGrid();
@@ -69,8 +80,9 @@ function play() {
     const clearBtn = document.querySelector('#clear-grid');
     clearBtn.addEventListener('click', clearGrid);
 
+    // need to change the way I change the pen's color
     const randomColorsBtn = document.querySelector('#random-colors');
-    randomColorsBtn.addEventListener('click', )
+    randomColorsBtn.addEventListener('click', useRandomColor);
 }
 
 play();
